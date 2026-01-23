a4fbcee9c5e99e54ee937716b86f85cd51a13e90
Ahmad Muhammad <theahmadm3@gmail.com>
2026-01-23 17:04:45 +0100

    Add background color to CustomerLayout for improved visibility

--- src/app/customer/layout.tsx
+++ src/app/customer/layout.tsx
@@ -139,7 +139,7 @@ export default function CustomerLayout({
 					</SidebarFooter>
 				</Sidebar>
 
-				<div className="flex-1 flex flex-col min-w-0">
+				<div className="flex-1 flex flex-col min-w-0 bg-white">
 					{showHeader && <ClientHeader />}
 					<main className="flex-1 overflow-y-auto hide-scrollbar">
 						<div className="md:p-6 lg:p-8 pb-20 md:pb-8">{children}</div>
